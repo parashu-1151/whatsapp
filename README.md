f8ff7ac6957d1189e3272c8403f5c49e6cfe92c7
EC2 Default User <ec2-user@ip-172-31-5-182.ap-south-1.compute.internal>
2020-01-27 10:01:16 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center"> whatsapp </h1>
+
+[![Project Status](http://www.repostatus.org/badges/latest/active.svg)](http://www.repostatus.org/#active)
+[![Build Status](https://travis-ci.org/githubuser/whatsapp.svg?branch=master)](https://travis-ci.org/githubuser/whatsapp)
+[![codecov](https://codecov.io/gh/githubuser/whatsapp/branch/master/graph/badge.svg)](https://codecov.io/gh/githubuser/whatsapp)
+[![Codacy Badge]()]()
+
+description
+
+<h2>Introduction</h2>
+
+TODO
+
+<h2>Usage</h2>
+
+TODO
+
+<h2>Author</h2>
+
+* user <a href="mailto:user@noreply.net">user@noreply.net</a>
---
+++ src/main/java/techparashu/whatsapp/Main.java
@@ -0,0 +1,35 @@
+/**
+ * whatsapp: description
+ * <p>
+ * Copyright (C) user
+ * <p>
+ * This file is part of whatsapp.
+ * <p>
+ * whatsapp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * whatsapp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with whatsapp. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package techparashu.whatsapp;
+
+
+/**
+ * @author user {@literal user@noreply.net}
+ */
+public final class Main
+{
+    public static void main(String[] args)
+    {
+    	System.out.println("Have fun with java-bones");
+    }
+}
